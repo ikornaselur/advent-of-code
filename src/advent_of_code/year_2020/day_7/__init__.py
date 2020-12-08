c5b6235d959e92e668b1b90f691096bebde68489
Axel Örn <dev@absalon.is>
2020-12-08 21:29:23 +0000

    Solve 2020 - Day 7 - Part 2

--- src/advent_of_code/year_2020/day_7/__init__.py
+++ src/advent_of_code/year_2020/day_7/__init__.py
@@ -1,9 +1,15 @@
 import re
+from functools import lru_cache
 from typing import Dict, List, Set, Tuple
 
 from advent_of_code import Problem
 
-PATTERN = re.compile(r"\d+ ([\w\s]+) bags?")
+CONTAIN_PATTERN = re.compile(r"\d+ ([\w\s]+) bags?")
+LEVEL_PATTERN = re.compile(r"(\d+) ([\w\s]+) bags?")
+
+##########
+# Part 1 #
+##########
 
 
 def parse_rule(rule: str) -> Tuple[str, List[str]]:
@@ -12,12 +18,12 @@ def parse_rule(rule: str) -> Tuple[str, List[str]]:
     if bags_str == "no other bags":
         bags = []
     else:
-        bags = PATTERN.findall(bags_str)
+        bags = CONTAIN_PATTERN.findall(bags_str)
 
     return container, bags
 
 
-def create_map(rules: List[Tuple[str, List[str]]]) -> Dict[str, Set[str]]:
+def create_contain_map(rules: List[Tuple[str, List[str]]]) -> Dict[str, Set[str]]:
     """ Create a map of potential parents """
     bag_map: Dict[str, Set[str]] = {}
 
@@ -31,7 +37,7 @@ def create_map(rules: List[Tuple[str, List[str]]]) -> Dict[str, Set[str]]:
 
 
 def can_contain(bag: str, rules: List[Tuple[str, List[str]]]) -> int:
-    bag_map = create_map(rules)
+    bag_map = create_contain_map(rules)
 
     top_level_bags: Set[str] = set()
     checked_bags: Set[str] = set()
@@ -53,6 +59,39 @@ def can_contain(bag: str, rules: List[Tuple[str, List[str]]]) -> int:
     return len(top_level_bags)
 
 
+##########
+# Part 2 #
+##########
+
+
+def parse_rule_level(rule: str) -> Tuple[str, List[Tuple[int, str]]]:
+    container, bags_str = rule.split(" bags contain ")
+
+    if bags_str == "no other bags":
+        bags = []
+    else:
+        bags = [(int(count), bag) for count, bag in LEVEL_PATTERN.findall(bags_str)]
+
+    return container, bags
+
+
+def bag_levels(bag: str, rules: List[Tuple[str, List[Tuple[int, str]]]]) -> int:
+    bag_map = dict(rules)
+
+    @lru_cache
+    def _bag_levels(bag: str) -> int:
+        sub_bags = bag_map[bag]
+
+        count = 0
+        for bag_count, sub_bag in sub_bags:
+            sub_bag_count = _bag_levels(sub_bag)
+            count += bag_count + bag_count * sub_bag_count
+
+        return count
+
+    return _bag_levels(bag)
+
+
 class Part1(Problem):
     def get_solution(self: "Part1") -> int:
         puzzle_input = [line.strip() for line in self.get_input(__file__)]
@@ -66,4 +105,6 @@ class Part2(Problem):
     def get_solution(self: "Part2") -> int:
         puzzle_input = self.get_input(__file__)
 
-        return len(puzzle_input) * 0
+        rules = list(map(parse_rule_level, puzzle_input))
+
+        return bag_levels("shiny gold", rules)
--- tests/unit/test_year_2020_day_7.py
+++ tests/unit/test_year_2020_day_7.py
@@ -1,4 +1,9 @@
-from advent_of_code.year_2020.day_7 import can_contain, parse_rule
+from advent_of_code.year_2020.day_7 import (
+    bag_levels,
+    can_contain,
+    parse_rule,
+    parse_rule_level,
+)
 
 
 def test_parse_rule() -> None:
@@ -35,3 +40,44 @@ def test_can_contan() -> None:
     )
 
     assert can_contain("shiny gold", rules) == 4
+
+
+def test_parse_rule_level() -> None:
+    assert parse_rule_level("faded blue bags contain no other bags.") == (
+        "faded blue",
+        [],
+    )
+    assert parse_rule_level("bright white bags contain 1 shiny gold bag.") == (
+        "bright white",
+        [(1, "shiny gold")],
+    )
+    assert parse_rule_level(
+        "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags."
+    ) == (
+        "vibrant plum",
+        [(5, "faded blue"), (6, "dotted black")],
+    )
+
+
+def test_bag_levels() -> None:
+    rules = list(
+        map(
+            parse_rule_level,
+            [
+                "light red bags contain 1 bright white bag, 2 muted yellow bags.",
+                "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+                "bright white bags contain 1 shiny gold bag.",
+                "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+                "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+                "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+                "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+                "faded blue bags contain no other bags.",
+                "dotted black bags contain no other bags.",
+            ],
+        )
+    )
+
+    assert bag_levels("faded blue", rules) == 0
+    assert bag_levels("dark olive", rules) == 7
+    assert bag_levels("vibrant plum", rules) == 11
+    assert bag_levels("shiny gold", rules) == 32
