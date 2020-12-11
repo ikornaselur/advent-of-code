0290660725b17778a0697af6a85dcc7f6f872994
Axel Örn <dev@absalon.is>
2020-12-11 14:37:12 +0000

    Solve 2020 - Day 10 - Part 2

--- src/advent_of_code/year_2020/day_10/__init__.py
+++ src/advent_of_code/year_2020/day_10/__init__.py
@@ -1,34 +1,91 @@
+from itertools import combinations
 from typing import List, Tuple, cast
 
 from advent_of_code import Problem
 
 
 def get_differences(joltages: List[int]) -> Tuple[int, int, int]:
+    return get_differences_from_section([0] + sorted(joltages) + [max(joltages) + 3])
+
+
+def get_differences_from_section(section: List[int]) -> Tuple[int, int, int]:
+    differences: List[int] = [0, 0, 0]
+
+    for idx in range(len(section) - 1):
+        differences[section[idx + 1] - section[idx] - 1] += 1
+
+    return cast(Tuple[int, int, int], tuple(differences))
+
+
+def get_sections(joltages_input: List[int]) -> List[List[int]]:
     # Add the charging outlet
-    joltages.append(0)
-    joltages.sort()
+    joltages = [0, *sorted(joltages_input)]
     # Add the device builtin joltage
     joltages.append(joltages[-1] + 3)
 
-    differences: List[int] = [0, 0, 0]
+    # Sections to look for combinations at
+    sections: List[List[int]] = []
 
-    for idx in range(len(joltages) - 1):
-        differences[joltages[idx + 1] - joltages[idx] - 1] += 1
+    current_section: List[int] = []
+    for idx, joltage in enumerate(joltages):
+        if not len(current_section):
+            if idx:
+                if joltage - joltages[idx - 1] == 3:
+                    continue
+                current_section.append(joltages[idx - 1])
 
-    return cast(Tuple[int, int, int], tuple(differences))
+            current_section.append(joltage)
+            continue
+
+        difference = joltage - current_section[-1]
+        if difference == 3:
+            if len(current_section) > 2:
+                sections.append(current_section)
+            current_section = []
+        else:
+            current_section.append(joltage)
+
+    return sections
+
+
+def count_arrangements(joltages_input: List[int]) -> int:
+    sections = get_sections(joltages_input)
+
+    arrangements = 1
+    for section in sections:
+        # Assume each section is short and just brute force each of them
+        section_arrangements = 0
+        sub_section = section[1:-1]
+        for length in range(1, len(sub_section) + 1):
+            for combination in combinations(section[1:-1], length):
+                try:
+                    get_differences_from_section([section[0], *combination, section[-1]])
+                except IndexError:
+                    # Not valid...
+                    continue
+                section_arrangements += 1
+        try:
+            get_differences_from_section([section[0], section[-1]])
+        except IndexError:
+            pass
+        else:
+            section_arrangements += 1
+        arrangements *= section_arrangements
+
+    return arrangements
 
 
 class Part1(Problem):
     def get_solution(self: "Part1") -> int:
         puzzle_input = [int(line) for line in self.get_input(__file__)]
 
-        one, two, three = get_differences(puzzle_input)
+        one, _two, three = get_differences(puzzle_input)
 
         return one * three
 
 
 class Part2(Problem):
     def get_solution(self: "Part2") -> int:
-        puzzle_input = self.get_input(__file__)
+        puzzle_input = [int(line) for line in self.get_input(__file__)]
 
-        return len(puzzle_input) * 0
+        return count_arrangements(puzzle_input)
--- tests/unit/year_2020/test_year_2020_day_10.py
+++ tests/unit/year_2020/test_year_2020_day_10.py
@@ -1,22 +1,8 @@
-from advent_of_code.year_2020.day_10 import get_differences
+from advent_of_code.year_2020.day_10 import count_arrangements, get_differences
 
 
-def test_get_differences_basic() -> None:
-    joltages = [
-        16,
-        10,
-        15,
-        5,
-        1,
-        11,
-        7,
-        19,
-        6,
-        12,
-        4,
-    ]
-
-    one, two, three = get_differences(joltages)
+def test_get_differences_basic_example() -> None:
+    one, two, three = get_differences(BASIC_JOLTAGES)
 
     assert one == 7
     assert two == 0
@@ -24,42 +10,65 @@ def test_get_differences_basic() -> None:
 
 
 def test_get_differences_bigger_example() -> None:
-    joltages = [
-        28,
-        33,
-        18,
-        42,
-        31,
-        14,
-        46,
-        20,
-        48,
-        47,
-        24,
-        23,
-        49,
-        45,
-        19,
-        38,
-        39,
-        11,
-        1,
-        32,
-        25,
-        35,
-        8,
-        17,
-        7,
-        9,
-        4,
-        2,
-        34,
-        10,
-        3,
-    ]
-
-    one, two, three = get_differences(joltages)
+    one, two, three = get_differences(BIGGER_JOLTAGES)
 
     assert one == 22
     assert two == 0
     assert three == 10
+
+
+def test_count_arrangements_basic_example() -> None:
+    assert count_arrangements(BASIC_JOLTAGES) == 8
+
+
+def test_count_arrangements_bigger_example() -> None:
+    assert count_arrangements(BIGGER_JOLTAGES) == 19208
+
+
+BASIC_JOLTAGES = [
+    16,
+    10,
+    15,
+    5,
+    1,
+    11,
+    7,
+    19,
+    6,
+    12,
+    4,
+]
+
+BIGGER_JOLTAGES = [
+    28,
+    33,
+    18,
+    42,
+    31,
+    14,
+    46,
+    20,
+    48,
+    47,
+    24,
+    23,
+    49,
+    45,
+    19,
+    38,
+    39,
+    11,
+    1,
+    32,
+    25,
+    35,
+    8,
+    17,
+    7,
+    9,
+    4,
+    2,
+    34,
+    10,
+    3,
+]
