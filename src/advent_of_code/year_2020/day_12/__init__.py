400fab51777159bf33641c8a350177ae4f3043bb
Axel Örn <dev@absalon.is>
2020-12-13 18:34:27 +0000

    Solve 2020 - Day 12 - Part 2

--- src/advent_of_code/year_2020/day_12/__init__.py
+++ src/advent_of_code/year_2020/day_12/__init__.py
@@ -43,6 +43,58 @@ class ShipPosition:
         return f"<ShipPosition: ({self.x},{self.y}) - facing {facing[self.direction]}"
 
 
+@dataclass
+class WaypointPosition:
+    ship_x: int
+    ship_y: int
+    waypoint_x: int
+    waypoint_y: int
+
+    def move(self: "WaypointPosition", instruction: str) -> None:
+        action = instruction[:1]
+        amount = int(instruction[1:])
+
+        if action == "N":
+            self.waypoint_x += amount
+        elif action == "E":
+            self.waypoint_y += amount
+        elif action == "S":
+            self.waypoint_x -= amount
+        elif action == "W":
+            self.waypoint_y -= amount
+        elif action == "L":
+            if amount // 90 % 4 == 1:
+                self.waypoint_x, self.waypoint_y = self.waypoint_y, self.waypoint_x * -1
+            elif amount // 90 % 4 == 2:
+                self.waypoint_x *= -1
+                self.waypoint_y *= -1
+            elif amount // 90 % 4 == 3:
+                self.waypoint_x, self.waypoint_y = self.waypoint_y * -1, self.waypoint_x
+            else:
+                raise ValueError(f"Invalid amount for rotation: {amount}")
+        elif action == "R":
+            if amount // 90 % 4 == 1:
+                self.waypoint_x, self.waypoint_y = self.waypoint_y * -1, self.waypoint_x
+            elif amount // 90 % 4 == 2:
+                self.waypoint_x *= -1
+                self.waypoint_y *= -1
+            elif amount // 90 % 4 == 3:
+                self.waypoint_x, self.waypoint_y = self.waypoint_y, self.waypoint_x * -1
+            else:
+                raise ValueError(f"Invalid amount for rotation: {amount}")
+        elif action == "F":
+            self.ship_x += self.waypoint_x * amount
+            self.ship_y += self.waypoint_y * amount
+        else:
+            raise ValueError(f"Unknown action: {action}")
+
+    def __repr__(self: "WaypointPosition") -> str:
+        return (
+            f"<WaypointPosition: Ship ({self.ship_x},{self.ship_y}) "
+            f"Waypoint ({self.waypoint_x},{self.waypoint_y})"
+        )
+
+
 class Part1(Problem):
     def get_solution(self: "Part1") -> int:
         puzzle_input = self.get_input(__file__)
@@ -59,4 +111,9 @@ class Part2(Problem):
     def get_solution(self: "Part2") -> int:
         puzzle_input = self.get_input(__file__)
 
-        return len(puzzle_input) * 0
+        position = WaypointPosition(0, 0, 1, 10)
+
+        for instruction in puzzle_input:
+            position.move(instruction)
+
+        return abs(position.ship_x) + abs(position.ship_y)
--- tests/unit/year_2020/test_year_2020_day_12.py
+++ tests/unit/year_2020/test_year_2020_day_12.py
@@ -1,7 +1,7 @@
-from advent_of_code.year_2020.day_12 import ShipPosition
+from advent_of_code.year_2020.day_12 import ShipPosition, WaypointPosition
 
 
-def test_move() -> None:
+def test_move_ship_position() -> None:
     pos = ShipPosition(1, 2, 3)
 
     pos.move("N10")
@@ -27,3 +27,22 @@ def test_move() -> None:
 
     pos.move("F1")
     assert pos == ShipPosition(8, 6, 0)
+
+
+def test_move_waypoint_position() -> None:
+    pos = WaypointPosition(0, 0, 1, 10)
+
+    pos.move("F10")
+    assert pos == WaypointPosition(10, 100, 1, 10)
+
+    pos.move("N3")
+    assert pos == WaypointPosition(10, 100, 4, 10)
+
+    pos.move("F7")
+    assert pos == WaypointPosition(38, 170, 4, 10)
+
+    pos.move("R90")
+    assert pos == WaypointPosition(38, 170, -10, 4)
+
+    pos.move("F11")
+    assert pos == WaypointPosition(-72, 214, -10, 4)
