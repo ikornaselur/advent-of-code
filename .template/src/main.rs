2ff31c02e6bb1173578ef18eff58c9f985baf718
Axel Örn Sigurðsson <git@absalon.dev>
2023-12-03 13:57:28 +0000

    chore: Update template to use new error handling

--- .template/src/main.rs
+++ .template/src/main.rs
@@ -1,19 +1,23 @@
+use advent_core::error::AdventError;
+
 const INPUT: &str = include_str!("../input.txt");
 
-fn main() {
+fn main() -> Result<(), AdventError> {
     println!("## Part 1");
-    println!(" > {}", part1(INPUT));
+    println!(" > {}", part1(INPUT)?);
 
     println!("## Part 2");
-    println!(" > {}", part2(INPUT));
+    println!(" > {}", part2(INPUT)?);
+
+    Ok(())
 }
 
-fn part1(input: &str) -> u32 {
-    0
+fn part1(input: &str) -> Result<u32, AdventError> {
+    Ok(0)
 }
 
-fn part2(input: &str) -> u32 {
-    0
+fn part2(input: &str) -> Result<u32, AdventError> {
+    Ok(0)
 }
 
 #[cfg(test)]
@@ -25,11 +29,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(PART_1_TEST_INPUT), 0);
+        assert_eq!(part1(PART_1_TEST_INPUT).unwrap(), 0);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(PART_2_TEST_INPUT), 0);
+        assert_eq!(part2(PART_2_TEST_INPUT).unwrap(), 0);
     }
 }
