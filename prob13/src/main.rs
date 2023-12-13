7b44165638f28473a1525f21b7d3492ed83c2882
Axel Örn Sigurðsson <git@absalon.dev>
2023-12-13 19:56:40 +0000

    prob 13: Solve part 2

--- prob13/src/main.rs
+++ prob13/src/main.rs
@@ -5,70 +5,62 @@ const INPUT: &str = include_str!("../input.txt");
 
 #[derive(Debug)]
 struct Pattern {
-    pattern: Vec<Vec<Node>>,
-    rows: Vec<u64>,
-    cols: Vec<u64>,
+    rows: Vec<u32>,
+    cols: Vec<u32>,
 }
 
 impl FromStr for Pattern {
     type Err = AdventError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let pattern = s
-            .lines()
-            .map(|line| line.chars().map(Node::from).collect::<Vec<Node>>())
-            .collect::<Vec<Vec<Node>>>();
+        let lines: Vec<_> = s.lines().collect();
 
         // Since the input is just ash or rocks, we can represent it as bits in a 64 bit binary
         // number
-        let rows: Vec<u64> = pattern
+        let rows: Vec<u32> = lines
             .iter()
-            .map(|row| {
-                row.iter()
-                    .map(|node| match node {
-                        Node::Ash => 0,
-                        Node::Rock => 1,
+            .map(|line| {
+                line.chars()
+                    .map(|c| match c {
+                        '.' => 0,
+                        '#' => 1,
+                        _ => panic!("Unknown node type"),
                     })
                     .fold(0, |acc, node| (acc << 1) | node)
             })
             .collect();
 
-        let cols: Vec<u64> = (0..pattern[0].len())
+        let cols: Vec<u32> = (0..lines[0].len())
             .map(|col| {
-                pattern
+                lines
                     .iter()
-                    .map(|row| match row[col] {
-                        Node::Ash => 0,
-                        Node::Rock => 1,
+                    .map(|line| match line.chars().nth(col) {
+                        Some('.') => 0,
+                        Some('#') => 1,
+                        _ => panic!("Unknown node type"),
                     })
                     .fold(0, |acc, node| (acc << 1) | node)
             })
             .collect();
-        Ok(Pattern {
-            pattern,
-            rows,
-            cols,
-        })
+        Ok(Pattern { rows, cols })
     }
 }
 
 impl Pattern {
-    fn get_mirror_value(&self, values: &[u64]) -> Option<usize> {
+    fn get_mirror_value(&self, values: &[u32], smudges: u32) -> Option<usize> {
         // Start by finding any pairs of numbers in the rows, by zipping the rows with it self
         // offset by 1
         let pairs: Vec<_> = values
             .iter()
             .zip(values.iter().skip(1))
             .enumerate()
-            .filter_map(
-                |(i, (row1, row2))| {
-                    if row1 == row2 {
-                        Some(i)
-                    } else {
-                        None
-                    }
-                },
-            )
+            .filter_map(|(i, (row1, row2))| {
+                if (row1 ^ row2).count_ones() <= smudges {
+                    Some(i)
+                } else {
+                    None
+                }
+            })
             .collect();
 
         if pairs.is_empty() {
@@ -80,12 +72,21 @@ impl Pattern {
             let mut left = *left_id;
             let mut right = left + 1;
 
+            let mut smudges = smudges;
+
             loop {
-                if values[left] != values[right] {
+                let ones = (values[left] ^ values[right]).count_ones();
+                if smudges > 0 && ones > 0 && ones <= smudges {
+                    smudges -= ones;
+                } else if ones != 0 {
                     // We found a pair that doesn't match, we can continue onto next pair
                     continue 'outer;
                 }
                 if left == 0 || right == values.len() - 1 {
+                    if smudges > 0 {
+                        // We are looking at a pattern that is still smudged
+                        continue 'outer;
+                    }
                     // We reached the edge of the pattern, without breaking the search, meaning the
                     // pattern is mirrored
                     return Some(left_id + 1);
@@ -97,32 +98,18 @@ impl Pattern {
 
         None
     }
+
     /// Find the horizontal mirror row
     ///
     /// If the pattern is not mirrored horizontally, return None
     /// otherwise return the row index where the mirror is
-    fn get_horizontal_mirror_value(&self) -> Option<usize> {
-        self.get_mirror_value(&self.rows).map(|row| row * 100)
+    fn get_horizontal_mirror_value(&self, smudges: u32) -> Option<usize> {
+        self.get_mirror_value(&self.rows, smudges)
+            .map(|row| row * 100)
     }
 
-    fn get_vertical_mirror_value(&self) -> Option<usize> {
-        self.get_mirror_value(&self.cols)
-    }
-}
-
-#[derive(Debug, PartialEq)]
-enum Node {
-    Ash,  // .
-    Rock, // #
-}
-
-impl From<char> for Node {
-    fn from(c: char) -> Self {
-        match c {
-            '#' => Node::Rock,
-            '.' => Node::Ash,
-            _ => panic!("Unknown node type"),
-        }
+    fn get_vertical_mirror_value(&self, smudges: u32) -> Option<usize> {
+        self.get_mirror_value(&self.cols, smudges)
     }
 }
 
@@ -153,15 +140,23 @@ fn part1(input: &str) -> Result<usize, AdventError> {
     let total = patterns
         .iter()
         .map(|pattern| {
-            pattern.get_vertical_mirror_value().unwrap_or(0)
-                + pattern.get_horizontal_mirror_value().unwrap_or(0)
+            pattern.get_vertical_mirror_value(0).unwrap_or(0)
+                + pattern.get_horizontal_mirror_value(0).unwrap_or(0)
         })
         .sum::<usize>();
     Ok(total)
 }
 
-fn part2(input: &str) -> Result<u32, AdventError> {
-    Ok(0)
+fn part2(input: &str) -> Result<usize, AdventError> {
+    let patterns: Vec<Pattern> = parse_input(input)?;
+    let total = patterns
+        .iter()
+        .map(|pattern| {
+            pattern.get_vertical_mirror_value(1).unwrap_or(0)
+                + pattern.get_horizontal_mirror_value(1).unwrap_or(0)
+        })
+        .sum::<usize>();
+    Ok(total)
 }
 
 #[cfg(test)]
@@ -177,17 +172,12 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(TEST_INPUT).unwrap(), 0);
+        assert_eq!(part2(TEST_INPUT).unwrap(), 400);
     }
 
     #[test]
     fn test_pattern_from_str() {
         let pattern: Pattern = "#.#\n...\n###".parse().unwrap();
-
-        assert_eq!(pattern.pattern.len(), 3);
-        assert_eq!(pattern.pattern[0], vec![Node::Rock, Node::Ash, Node::Rock]);
-        assert_eq!(pattern.pattern[1], vec![Node::Ash, Node::Ash, Node::Ash]);
-        assert_eq!(pattern.pattern[2], vec![Node::Rock, Node::Rock, Node::Rock]);
 
         assert_eq!(pattern.rows, vec![5, 0, 7]);
         assert_eq!(pattern.cols, vec![5, 1, 5]);
@@ -203,28 +193,41 @@ mod tests {
     #[test]
     fn test_get_horizontal_mirror_row() {
         let pattern: Pattern = "#.#\n...\n...\n###".parse().unwrap();
-        assert_eq!(pattern.get_horizontal_mirror_value(), None);
+        assert_eq!(pattern.get_horizontal_mirror_value(0), None);
 
         let pattern: Pattern = "#.#\n...\n...\n#.#".parse().unwrap();
-        assert_eq!(pattern.get_horizontal_mirror_value(), Some(200));
+        assert_eq!(pattern.get_horizontal_mirror_value(0), Some(200));
 
         let test_patterns: Vec<Pattern> = parse_input(TEST_INPUT).unwrap();
 
-        assert_eq!(test_patterns[0].get_horizontal_mirror_value(), None);
-        assert_eq!(test_patterns[1].get_horizontal_mirror_value(), Some(400));
+        assert_eq!(test_patterns[0].get_horizontal_mirror_value(0), None);
+        assert_eq!(test_patterns[1].get_horizontal_mirror_value(0), Some(400));
     }
 
     #[test]
     fn test_get_vertical_mirror_row() {
         let pattern: Pattern = "#..#\n....\n....\n#.##".parse().unwrap();
-        assert_eq!(pattern.get_vertical_mirror_value(), None);
+        assert_eq!(pattern.get_vertical_mirror_value(0), None);
 
         let pattern: Pattern = "#..#\n....\n....\n#..#".parse().unwrap();
-        assert_eq!(pattern.get_vertical_mirror_value(), Some(2));
+        assert_eq!(pattern.get_vertical_mirror_value(0), Some(2));
 
         let test_patterns: Vec<Pattern> = parse_input(TEST_INPUT).unwrap();
 
-        assert_eq!(test_patterns[0].get_vertical_mirror_value(), Some(5));
-        assert_eq!(test_patterns[1].get_vertical_mirror_value(), None);
+        assert_eq!(test_patterns[0].get_vertical_mirror_value(0), Some(5));
+        assert_eq!(test_patterns[1].get_vertical_mirror_value(0), None);
+    }
+
+    #[test]
+    fn test_get_smudged_mirror_row() {
+        let test_patterns: Vec<Pattern> = parse_input(TEST_INPUT).unwrap();
+        assert_eq!(
+            test_patterns[0].get_mirror_value(&test_patterns[0].rows, 1),
+            Some(3)
+        );
+        assert_eq!(
+            test_patterns[1].get_mirror_value(&test_patterns[1].rows, 1),
+            Some(1)
+        );
     }
 }
