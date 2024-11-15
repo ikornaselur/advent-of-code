2ba86e71cf5fe5cc4de62b09a8db7700b4e02891
Axel Örn Sigurðsson <git@absalon.dev>
2024-11-15 20:40:51 +0100

    [2022] prob 5: Solve part 1

---
+++ year2022/prob05/src/main.rs
@@ -0,0 +1,72 @@
+use advent::prelude::*;
+
+mod parse;
+
+const INPUT: &str = include_str!("../input.txt");
+
+#[derive(Debug, Clone)]
+struct Stack {
+    crates: Vec<char>,
+}
+
+#[derive(Debug)]
+struct Instruction {
+    from: usize,
+    to: usize,
+    count: usize,
+}
+
+fn main() -> Result<()> {
+    println!("## Part 1");
+    println!(" > {}", part1(INPUT)?);
+
+    println!("## Part 2");
+    println!(" > {}", part2(INPUT)?);
+
+    Ok(())
+}
+
+fn part1(input: &str) -> Result<String> {
+    let (mut stacks, instructions) = parse::parse_input(input)?;
+
+    // Each instruction tells us how many crates to move from one stack to another, so an
+    // instruction 'move 1 from 2 to 1' means "move 1 crate from stack 2 onto stack 1"
+    // Crates are moved *one at a time*, meaning that if an instruction says 'move 2 from 3 to 1'
+    // it means to move the top crate from stack 3 to stack 1, then move the next top crate.
+    for instruction in instructions {
+        let from_idx = instruction.from - 1;
+        let to_idx = instruction.to - 1;
+
+        for _ in 0..instruction.count {
+            let crate_ = stacks[from_idx].crates.pop().unwrap();
+            stacks[to_idx].crates.push(crate_);
+        }
+    }
+
+    // Finally we go through the stacks and construct a string of the top element of each
+    Ok(stacks
+        .iter()
+        .map(|stack| stack.crates.last().unwrap())
+        .collect::<String>())
+}
+
+fn part2(_input: &str) -> Result<u32> {
+    Ok(0)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = include_str!("../test.txt");
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(TEST_INPUT).unwrap(), String::from("CMZ"));
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(TEST_INPUT).unwrap(), 0);
+    }
+}
---
+++ year2022/prob05/src/parse.rs
@@ -0,0 +1,186 @@
+use advent::prelude::*;
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take},
+    character::complete::{char, digit1, line_ending, multispace0, space0},
+    multi::separated_list1,
+    sequence::{delimited, preceded},
+    IResult,
+};
+
+use crate::{Instruction, Stack};
+
+/// Nom a single crate spot
+///
+/// Return none if empty, otherwise the character that was in the spot
+fn nom_crate(input: &str) -> IResult<&str, Option<&str>> {
+    let empty_parser = tag("   ");
+    let crate_parser = delimited(char('['), take(1u8), char(']'));
+
+    let (input, raw_crate) = alt((empty_parser, crate_parser))(input)?;
+
+    match raw_crate {
+        "   " => Ok((input, None)),
+        c => Ok((input, Some(c))),
+    }
+}
+
+/// Nom a line that looks like this:
+///
+///     [A] [B]     [C] [D]
+///
+/// which should return (assuming A is start of the line):
+///
+///     Some('A'), Some('B'), None, Some('C'), Some('D')
+///
+/// These are then used to create a list of stacks
+fn nom_crate_row(input: &str) -> IResult<&str, Vec<Option<&str>>> {
+    let mut crates_parser = separated_list1(char(' '), nom_crate);
+
+    let (input, crates) = crates_parser(input)?;
+
+    Ok((input, crates))
+}
+
+fn nom_all_crate_rows(input: &str) -> IResult<&str, Vec<Vec<Option<&str>>>> {
+    let mut line_parser = separated_list1(line_ending, nom_crate_row);
+
+    let (input, crates) = line_parser(input)?;
+    let (input, _) = preceded(space0, line_ending)(input)?; // Get rid of that final line
+
+    Ok((input, crates))
+}
+
+fn nom_column_count(input: &str) -> IResult<&str, usize> {
+    let mut count_parser = preceded(multispace0, separated_list1(multispace0, digit1));
+
+    let (input, counts) = count_parser(input)?;
+    let (input, _) = preceded(space0, line_ending)(input)?; // Get rid of that final line
+
+    // Get the last element of counts
+    Ok((input, counts.last().unwrap().parse().unwrap()))
+}
+
+fn nom_instruction(input: &str) -> IResult<&str, (usize, usize, usize)> {
+    let (input, _) = tag("move ")(input)?;
+    let (input, crate_count) = digit1(input)?;
+    let (input, _) = tag(" from ")(input)?;
+    let (input, from_stack) = digit1(input)?;
+    let (input, _) = tag(" to ")(input)?;
+    let (input, to_stack) = digit1(input)?;
+
+    Ok((
+        input,
+        (
+            crate_count.parse().unwrap(),
+            from_stack.parse().unwrap(),
+            to_stack.parse().unwrap(),
+        ),
+    ))
+}
+
+fn nom_instructions(input: &str) -> IResult<&str, Vec<(usize, usize, usize)>> {
+    let mut instruction_parser = separated_list1(line_ending, nom_instruction);
+    let (input, instructions) = instruction_parser(input)?;
+    let (input, _) = preceded(space0, line_ending)(input)?; // Get rid of that final line
+    Ok((input, instructions))
+}
+
+/// Parse the input for the puzzle
+///
+/// The input will be parsed into a tuple of two elements. The first element is a list of stacks,
+/// The second element element is a list of instructions.
+pub fn parse_input(input: &str) -> Result<(Vec<Stack>, Vec<Instruction>)> {
+    // Parse the crate rows
+    let (input, crate_rows) =
+        nom_all_crate_rows(input).map_err(|e| error!("Unable to parse: {}", e))?;
+    // Parse the column count
+    let (input, column_count) =
+        nom_column_count(input).map_err(|e| error!("Unable to parse: {}", e))?;
+    // There's an empty line before instructions
+    let (input, _) = preceded::<_, _, _, nom::error::Error<_>, _, _>(space0, line_ending)(input)
+        .map_err(|e| error!("Unable to parse {}", e))?;
+    // And finally all the instructions
+    let (_, instructions) =
+        nom_instructions(input).map_err(|e| error!("Unable to parse: {}", e))?;
+
+    // Create the stacks
+    let mut stacks: Vec<Stack> = vec![Stack { crates: vec![] }; column_count];
+
+    // Then populate the stacks, by going through the rows in reverse order
+    for row in crate_rows.iter().rev() {
+        for (i, crate_) in row.iter().enumerate() {
+            if let Some(crate_) = crate_ {
+                stacks[i].crates.push(crate_.chars().next().unwrap());
+            }
+        }
+    }
+
+    // And finally just parse the instructions
+    let instructions = instructions
+        .into_iter()
+        .map(|(count, from, to)| Instruction { count, from, to })
+        .collect();
+
+    Ok((stacks, instructions))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = include_str!("../test.txt");
+
+    #[test]
+    fn test_nom_crate() {
+        assert_eq!(nom_crate("[A]"), Ok(("", Some("A"))));
+        assert_eq!(nom_crate("   "), Ok(("", None)));
+    }
+
+    #[test]
+    fn test_nom_crate_row() {
+        assert_eq!(
+            nom_crate_row("[A] [B]     [C] [D]"),
+            Ok(("", vec![Some("A"), Some("B"), None, Some("C"), Some("D"),]))
+        );
+    }
+
+    #[test]
+    fn test_nom_all_crate_rows() {
+        let lines = [
+            "[A] [B]     [C] [D]",
+            "[E] [F] [G] [H] [I]",
+            " 1   2   3   4   5",
+        ];
+        let input = lines.join("\n");
+
+        assert_eq!(
+            nom_all_crate_rows(&input),
+            Ok((
+                " 1   2   3   4   5",
+                vec![
+                    vec![Some("A"), Some("B"), None, Some("C"), Some("D")],
+                    vec![Some("E"), Some("F"), Some("G"), Some("H"), Some("I")],
+                ]
+            ))
+        );
+    }
+
+    #[test]
+    fn test_nom_column_count() {
+        assert_eq!(nom_column_count(" 1   2   3   4   5\n"), Ok(("", 5)));
+    }
+
+    #[test]
+    fn test_nom_instruction() {
+        assert_eq!(nom_instruction("move 2 from 8 to 4"), Ok(("", (2, 8, 4))));
+    }
+
+    #[test]
+    fn test_parse_input() {
+        let (stacks, instructions) = parse_input(TEST_INPUT).unwrap();
+
+        assert_eq!(stacks.len(), 3);
+        assert_eq!(stacks[0].crates.len(), 2);
+    }
+}
