fb8ac44fb3c7feac13c53fced5db5ec7c218bb90
Axel Örn Sigurðsson <git@absalon.dev>
2024-12-02 09:13:47 +0100

    [2024] prob 2: Solve part 2

--- year2024/prob02/src/main.rs
+++ year2024/prob02/src/main.rs
@@ -4,6 +4,7 @@ use parse::parse_input;
 mod parse;
 
 const INPUT: &str = include_str!("../input.txt");
+const MAX_LEVEL: i32 = 3;
 
 fn main() -> Result<()> {
     println!("## Part 1");
@@ -15,25 +16,47 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn part1(input: &str) -> Result<u32> {
-    let reports = parse_input(input)?;
-    let mut safe_reports = 0;
-    for report in reports {
-        // Check if the levels are either all increasing or all decreasing
-        let diffs = report.windows(2).map(|w| w[1] - w[0]).collect::<Vec<_>>();
-        let increasing = diffs.iter().all(|&x| x > 0);
-        let decreasing = diffs.iter().all(|&x| x < 0);
-        let max_diff = diffs.iter().map(|d| d.abs()).max().unwrap();
-
-        if (increasing || decreasing) && max_diff <= 3 {
-            safe_reports += 1;
-        }
+fn is_safe(report: &[i32]) -> bool {
+    if report.len() < 2 {
+        return true;
     }
+    let mut prev_diff = report[1] - report[0];
+    if prev_diff == 0 {
+        return false; // Already not safe
+    }
+
+    for window in report.windows(2) {
+        let diff = window[1] - window[0];
+        if diff == 0 || diff.signum() != prev_diff.signum() || diff.abs() > MAX_LEVEL {
+            return false;
+        }
+        prev_diff = diff;
+    }
+    true
+}
+
+fn part1(input: &str) -> Result<usize> {
+    let reports = parse_input(input)?;
+    let safe_reports = reports.iter().filter(|&r| is_safe(r)).count();
     Ok(safe_reports)
 }
 
-fn part2(_input: &str) -> Result<u32> {
-    Ok(0)
+fn part2(input: &str) -> Result<usize> {
+    let reports = parse_input(input)?;
+
+    let safe_reports = reports
+        .iter()
+        .filter(|report| {
+            is_safe(report)
+                || (0..report.len()).any(|i| {
+                    let mut filtered_report = Vec::with_capacity(report.len() - 1);
+                    filtered_report.extend_from_slice(&report[..i]);
+                    filtered_report.extend_from_slice(&report[i + 1..]);
+                    is_safe(&filtered_report)
+                })
+        })
+        .count();
+    Ok(safe_reports)
 }
 
 #[cfg(test)]
@@ -49,6 +72,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(TEST_INPUT).unwrap(), 0);
+        assert_eq!(part2(TEST_INPUT).unwrap(), 4);
     }
 }
