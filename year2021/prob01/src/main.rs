9c6aa2185aaf3b1dfc8d560ed252a5c0bea89950
Axel Örn Sigurðsson <git@absalon.dev>
2024-12-03 00:15:29 +0100

    [2021] prob 1: Solve part 1

---
+++ year2021/prob01/src/main.rs
@@ -0,0 +1,42 @@
+use advent::prelude::*;
+use parse::parse_input;
+
+mod parse;
+
+const INPUT: &str = include_str!("../input.txt");
+
+fn main() -> Result<()> {
+    println!("## Part 1");
+    println!(" > {}", part1(INPUT)?);
+
+    println!("## Part 2");
+    println!(" > {}", part2(INPUT)?);
+
+    Ok(())
+}
+
+fn part1(input: &str) -> Result<usize> {
+    let values = parse_input(input)?;
+    Ok(values.windows(2).filter(|w| w[0] < w[1]).count())
+}
+
+fn part2(input: &str) -> Result<usize> {
+    Ok(0)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = include_str!("../test.txt");
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(TEST_INPUT).unwrap(), 7);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(TEST_INPUT).unwrap(), 0);
+    }
+}
---
+++ year2021/prob01/src/parse.rs
@@ -0,0 +1,23 @@
+use advent::prelude::*;
+
+fn nom_line(input: &str) -> IResult<&str, usize> {
+    map_res(digit1, |s: &str| s.parse::<usize>())(input)
+}
+
+pub fn parse_input(input: &str) -> Result<Vec<usize>> {
+    let (_, pairs) = separated_list1(newline, nom_line)(input)
+        .map_err(|e| AdventError::ParseError(format!("Failed to parse input: {:?}", e)))?;
+
+    Ok(pairs)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_input() {
+        let input = "199\n200\n201";
+        assert_eq!(parse_input(input).unwrap(), vec![199, 200, 201]);
+    }
+}
