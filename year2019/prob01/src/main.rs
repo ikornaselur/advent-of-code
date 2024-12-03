335f272d458a00cc8932282854d9fce9ebf876e9
Axel Örn Sigurðsson <git@absalon.dev>
2024-12-03 19:43:47 +0100

    [2019] prob 1: Solve part 2

--- year2019/prob01/src/main.rs
+++ year2019/prob01/src/main.rs
@@ -19,6 +19,18 @@ fn calc_fuel(mass: usize) -> usize {
     (mass / 3).saturating_sub(2)
 }
 
+/// We need to calculate how much fuel we need for the fuel, which will need more fuel by itself..
+/// continue calculating until we reach 0 (or below)
+fn calc_total_fuel(mass: usize) -> usize {
+    let mut fuel_left = calc_fuel(mass);
+    let mut total_fuel = fuel_left;
+    while fuel_left > 0 {
+        fuel_left = calc_fuel(fuel_left);
+        total_fuel += fuel_left;
+    }
+    total_fuel
+}
+
 fn part1(input: &str) -> Result<usize> {
     let masses = parse_input(input)?;
 
@@ -26,7 +38,9 @@ fn part1(input: &str) -> Result<usize> {
 }
 
 fn part2(input: &str) -> Result<usize> {
-    Ok(0)
+    let masses = parse_input(input)?;
+
+    Ok(masses.iter().map(|&mass| calc_total_fuel(mass)).sum())
 }
 
 #[cfg(test)]
@@ -42,6 +56,20 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(TEST_INPUT).unwrap(), 0);
+        assert_eq!(part2(TEST_INPUT).unwrap(), 51_316);
+    }
+
+    #[test]
+    fn test_calc_fuel() {
+        assert_eq!(calc_fuel(12), 2);
+        assert_eq!(calc_fuel(14), 2);
+        assert_eq!(calc_fuel(1969), 654);
+        assert_eq!(calc_fuel(100_756), 33_583);
+    }
+
+    #[test]
+    fn test_calc_fuel_for_fuel() {
+        assert_eq!(calc_total_fuel(1969), 966);
+        assert_eq!(calc_total_fuel(100_756), 50_346);
     }
 }
