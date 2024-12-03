2aa74da1a6317921cec50b57f3c3332b16bd90ac
Axel Örn Sigurðsson <git@absalon.dev>
2024-12-03 19:07:14 +0100

    [2020] prob 1: Solve part 2

--- year2020/prob01/src/main.rs
+++ year2020/prob01/src/main.rs
@@ -18,10 +18,14 @@ fn main() -> Result<()> {
 
 fn part1(input: &str) -> Result<usize> {
     let mut values = parse_input(input)?;
+
     values.sort_unstable();
+
     let target = 2020;
+
     let mut left = 0;
     let mut right = values.len() - 1;
+
     while left < right {
         let sum = values[left] + values[right];
         match sum.cmp(&target) {
@@ -30,12 +34,31 @@ fn part1(input: &str) -> Result<usize> {
             Ordering::Greater => right -= 1,
         }
     }
-    Ok(0)
+
+    panic!("No solution found");
 }
 
 fn part2(input: &str) -> Result<usize> {
-    let values = parse_input(input)?;
-    Ok(0)
+    let mut values = parse_input(input)?;
+
+    let target = 2020;
+
+    values.sort_unstable();
+
+    for i in 0..values.len() {
+        let mut left = i + 1;
+        let mut right = values.len() - 1;
+        while left < right {
+            let sum = values[i] + values[left] + values[right];
+            match sum.cmp(&target) {
+                Ordering::Equal => return Ok(values[i] * values[left] * values[right]),
+                Ordering::Less => left += 1,
+                Ordering::Greater => right -= 1,
+            }
+        }
+    }
+
+    panic!("No solution found");
 }
 
 #[cfg(test)]
@@ -51,6 +74,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(TEST_INPUT).unwrap(), 0);
+        assert_eq!(part2(TEST_INPUT).unwrap(), 241_861_950);
     }
 }
