0a5b943146cc63bd85c0035817271b54c91d50ed
Axel Örn Sigurðsson <git@absalon.dev>
2024-12-07 10:23:00 +0100

    [2024] prob 7: Solve part 2

--- year2024/prob07/src/main.rs
+++ year2024/prob07/src/main.rs
@@ -1,6 +1,5 @@
 use advent::prelude::*;
 use parse::parse_input;
-use std::ops::{Add, Mul};
 
 mod parse;
 
@@ -16,27 +15,34 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-type Op<T> = fn(T, T) -> T;
-
-fn search_for_sum(sum: usize, acc: usize, ops: &[Op<usize>], digits: &[usize]) -> bool {
+fn search_for_sum(sum: usize, acc: usize, digits: &[usize], with_concat: bool) -> bool {
     // Base case, we ran out of digits to use, have we found the sum?
     if digits.is_empty() {
         return sum == acc;
     }
 
     // Second case, has the accumulator grown beyond the sum?
-    // NOTE: This is potentially only valid in part 1 where we are using Addition and
-    // Multiplication only
     if acc > sum {
         return false;
     }
 
     let next_digit = digits[0];
 
-    // Try all the operations
-    for op in ops {
-        let new_acc = op(acc, next_digit);
-        if search_for_sum(sum, new_acc, ops, &digits[1..]) {
+    // Try adding next digit
+    if search_for_sum(sum, acc + next_digit, &digits[1..], with_concat) {
+        return true;
+    }
+
+    // Try multiplying next digit
+    if search_for_sum(sum, acc * next_digit, &digits[1..], with_concat) {
+        return true;
+    }
+
+    // If we are in part 2, we just concatenate the next number to the current accumulator
+    if with_concat {
+        let next_acc =
+            acc * 10_usize.pow(next_digit.checked_ilog10().unwrap_or(0) + 1) + next_digit;
+        if search_for_sum(sum, next_acc, &digits[1..], with_concat) {
             return true;
         }
     }
@@ -45,24 +51,24 @@ fn search_for_sum(sum: usize, acc: usize, ops: &[Op<usize>], digits: &[usize]) -
     false
 }
 
-fn test_for_mult_add(sum: usize, digits: &[usize]) -> bool {
-    let ops: [fn(usize, usize) -> usize; 2] = [Add::add, Mul::mul];
-
-    search_for_sum(sum, 0, &ops, digits)
-}
-
 fn part1(input: &str) -> Result<usize> {
     let rows = parse_input(input)?;
 
     Ok(rows
         .iter()
-        .filter(|(sum, digits)| test_for_mult_add(*sum, digits))
+        .filter(|(sum, digits)| search_for_sum(*sum, 0, digits, false))
         .map(|(sum, _)| *sum)
         .sum())
 }
 
-fn part2(_input: &str) -> Result<usize> {
-    Ok(0)
+fn part2(input: &str) -> Result<usize> {
+    let rows = parse_input(input)?;
+
+    Ok(rows
+        .iter()
+        .filter(|(sum, digits)| search_for_sum(*sum, 0, digits, true))
+        .map(|(sum, _)| *sum)
+        .sum())
 }
 
 #[cfg(test)]
@@ -83,19 +89,25 @@ mod tests {
 
     #[test]
     fn test_search_for_sum() {
-        let ops: [fn(usize, usize) -> usize; 2] = [Add::add, Mul::mul];
-
         // Valid
-        assert!(search_for_sum(190, 0, &ops, &[10, 19]));
-        assert!(search_for_sum(3267, 0, &ops, &[81, 40, 27]));
-        assert!(search_for_sum(292, 0, &ops, &[11, 6, 16, 20]));
+        assert!(search_for_sum(190, 0, &[10, 19], false));
+        assert!(search_for_sum(3267, 0, &[81, 40, 27], false));
+        assert!(search_for_sum(292, 0, &[11, 6, 16, 20], false));
 
         // Invalid
-        assert!(!search_for_sum(83, 0, &ops, &[17, 5]));
-        assert!(!search_for_sum(156, 0, &ops, &[15, 6]));
-        assert!(!search_for_sum(7290, 0, &ops, &[6, 8, 6, 15]));
-        assert!(!search_for_sum(161011, 0, &ops, &[16, 10, 13]));
-        assert!(!search_for_sum(192, 0, &ops, &[17, 8, 14]));
-        assert!(!search_for_sum(21037, 0, &ops, &[9, 7, 18, 13]));
+        assert!(!search_for_sum(83, 0, &[17, 5], false));
+        assert!(!search_for_sum(156, 0, &[15, 6], false));
+        assert!(!search_for_sum(7290, 0, &[6, 8, 6, 15], false));
+        assert!(!search_for_sum(161011, 0, &[16, 10, 13], false));
+        assert!(!search_for_sum(192, 0, &[17, 8, 14], false));
+        assert!(!search_for_sum(21037, 0, &[9, 7, 18, 13], false));
+    }
+
+    #[test]
+    fn test_search_for_sum_with_concat() {
+        // Valid
+        assert!(search_for_sum(156, 0, &[15, 6], true));
+        assert!(search_for_sum(7290, 0, &[6, 8, 6, 15], true));
+        assert!(search_for_sum(192, 0, &[17, 8, 14], true));
     }
 }
