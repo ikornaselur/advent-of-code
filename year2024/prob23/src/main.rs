3950757eb0dab1a81860481113d14ad5e3872d52
Axel Örn Sigurðsson <git@absalon.dev>
2025-01-27 14:38:02 +0100

    [2024] prob 23: Solve part 1

---
+++ year2024/prob23/src/main.rs
@@ -0,0 +1,115 @@
+use advent::prelude::*;
+use parse::parse_input;
+use std::env;
+use std::fmt;
+use std::fs;
+
+mod parse;
+
+#[derive(Debug, Eq, PartialEq, Hash, Clone, Ord, PartialOrd)]
+struct Computer {
+    id: (char, char),
+}
+
+impl fmt::Display for Computer {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}{}", self.id.0, self.id.1)
+    }
+}
+
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let input = if args.len() > 1 {
+        let file_name = &args[1];
+        fs::read_to_string(file_name)
+            .map_err(|e| error!("Failed to read file {}: {}", file_name, e))?
+    } else {
+        get_input(2024, 23)?
+    };
+
+    println!("## Part 1");
+    let result = run_with_timeout("Part 1", part1, &input)?;
+    println!(" > {}", result);
+
+    println!("## Part 2");
+    let result = run_with_timeout("Part 2", part2, &input)?;
+    println!(" > {}", result);
+
+    benchmark_parts(
+        |input| {
+            part1(input).unwrap();
+        },
+        |input| {
+            part2(input).unwrap();
+        },
+        &input,
+    );
+
+    Ok(())
+}
+
+fn part1(input: &str) -> Result<usize> {
+    let connections = parse_input(input)?;
+
+    let mut groups = HashMap::new();
+    for (from, to) in &connections {
+        groups.entry(from).or_insert_with(HashSet::new).insert(to);
+        groups.entry(to).or_insert_with(HashSet::new).insert(from);
+    }
+
+    // Let's find a set of three connected computers, where each node is connected to the other
+    // two only. We can do this by iterating through all the nodes, and checking if the original
+    // node is connected to any of the connected nodes... does this make sense?
+    // So for node A, we go through it's connected nodes, which we can call node B (that is, A->B)
+    // and then iterate all the nodes connected to B. Those nodes we can call C (that is, B->C).
+    // For each node C, we check if it's connected to A, which means we have a A->B->C->A (three
+    // nodes connect to each other)
+    //
+    // We'll be storing these triples as sorted tuples, so prevent duplicates (as A,B,C is the same
+    // as C,B,A in terms of a node grouping)
+    //
+    // Additionally.. we only care about triples which contain a node that starts with 't' in part 1
+    let mut triples = HashSet::new();
+
+    // For simplicity, based on the description above
+    // A: from
+    // B: to
+    // C: next
+    for (from, to) in &connections {
+        for next in groups.get(to).unwrap() {
+            if groups.get(next).unwrap().contains(from) {
+                // Skip if none of the nodes start with 't'
+                if from.id.0 != 't' && to.id.0 != 't' && next.id.0 != 't' {
+                    continue;
+                }
+                let mut triple = vec![from, to, next];
+                triple.sort();
+                triples.insert(triple);
+            }
+        }
+    }
+
+    Ok(triples.len())
+}
+
+fn part2(_input: &str) -> Result<usize> {
+    // let thing = parse_input(input)?;
+    Ok(0)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = include_str!("../test.txt");
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(TEST_INPUT).unwrap(), 7);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(TEST_INPUT).unwrap(), 0);
+    }
+}
---
+++ year2024/prob23/src/parse.rs
@@ -0,0 +1,52 @@
+use crate::Computer;
+use advent::prelude::*;
+
+fn nom_computer(input: &str) -> IResult<&str, Computer> {
+    map(tuple((anychar, anychar)), |(a, b)| Computer { id: (a, b) })(input)
+}
+
+fn nom_connection(input: &str) -> IResult<&str, (Computer, Computer)> {
+    separated_pair(nom_computer, char('-'), nom_computer)(input)
+}
+
+pub fn parse_input(input: &str) -> Result<Vec<(Computer, Computer)>> {
+    let (_, connections) = separated_list1(newline, nom_connection)(input.trim_end())
+        .map_err(|e| AdventError::ParseError(format!("Failed to parse input: {:?}", e)))?;
+
+    Ok(connections)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_nom_computer() {
+        assert_eq!(
+            nom_computer("ab-cd"),
+            Ok(("-cd", Computer { id: ('a', 'b') }))
+        );
+    }
+
+    #[test]
+    fn test_nom_connection() {
+        assert_eq!(
+            nom_connection("ab-cd"),
+            Ok((
+                "",
+                (Computer { id: ('a', 'b') }, Computer { id: ('c', 'd') })
+            ))
+        );
+    }
+
+    #[test]
+    fn test_parse_input() {
+        assert_eq!(
+            parse_input("ab-cd\nab-ef\n").unwrap(),
+            vec![
+                (Computer { id: ('a', 'b') }, Computer { id: ('c', 'd') }),
+                (Computer { id: ('a', 'b') }, Computer { id: ('e', 'f') }),
+            ]
+        );
+    }
+}
