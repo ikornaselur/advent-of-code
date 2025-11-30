e14f465cfd57f1bdaa2ed1cdcbceb02b4927659b
Axel Örn Sigurðsson <git@absalon.dev>
2025-12-01 00:22:43 +0100

    Prep for 2025

---
+++ year2025/prob01/src/main.rs
@@ -0,0 +1,63 @@
+use advent::prelude::*;
+use parse::parse_input;
+use std::env;
+use std::fs;
+
+mod parse;
+
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let input = if args.len() > 1 {
+        let file_name = &args[1];
+        fs::read_to_string(file_name).map_err(|e| error!("Failed to read file {}: {}", file_name, e))?
+    } else {
+        get_input(2025, 1)?
+    };
+
+    println!("## Part 1");
+    let result = run_with_timeout("Part 1", part1, &input)?;
+    println!(" > {}", result);
+
+    println!("## Part 2");
+    let result = run_with_timeout("Part 2", part2, &input)?;
+    println!(" > {}", result);
+
+    benchmark_parts(
+        |input| {
+            part1(input).unwrap();
+        },
+        |input| {
+            part2(input).unwrap();
+        },
+        &input,
+    );
+
+    Ok(())
+}
+
+fn part1(_input: &str) -> Result<usize> {
+    // let thing = parse_input(input)?;
+    Ok(0)
+}
+
+fn part2(_input: &str) -> Result<usize> {
+    // let thing = parse_input(input)?;
+    Ok(0)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = include_str!("../test.txt");
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(TEST_INPUT).unwrap(), 0);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(TEST_INPUT).unwrap(), 0);
+    }
+}
---
+++ year2025/prob01/src/parse.rs
@@ -0,0 +1,7 @@
+use advent::prelude::*;
+
+pub fn parse_input(input: &str) -> Result<()> {
+    // let (_, digit) = digit1(input)?;
+
+    Ok(())
+}
