a5ca006e68931e28e88800c1f4cc3d153c90a500
Axel Örn Sigurðsson <git@absalon.dev>
2025-12-25 11:42:10 +0000

    [2025] prob 7: Refactor to prep for memoization

--- year2025/prob07/src/main.rs
+++ year2025/prob07/src/main.rs
@@ -29,10 +29,15 @@ type Col = usize;
 type Row = usize;
 type Coord = (Row, Col);
 
+struct Quantum(bool);
+
+#[allow(dead_code)]
 struct Grid {
     inner: Vec<Nodes>,
-    active_beams: Vec<Coord>,
-    beam_splits: usize,
+    start: Coord,
+    width: usize,
+    height: usize,
+    memo_grid: Vec<Option<usize>>,
 }
 
 impl Grid {
@@ -45,45 +50,79 @@ impl Grid {
         self.inner[0].len()
     }
 
-    fn beam_tick(&mut self) -> Result<()> {
-        // TODO: Review the clone, self.set_node_at_coord could mutably borrow just self.inner
-        // instead of self
-        let mut new_active_beams = Vec::new();
-        for (beam_row, beam_col) in self.active_beams.clone() {
-            match self.get_node_at_coord(&(beam_row, beam_col)) {
-                Some(node) if *node == Node::Start || *node == Node::Beam => node,
-                _ => panic!("Invalid node to tick"),
-            };
+    #[inline]
+    fn contains_key(&self, row: usize, col: usize) -> bool {
+        self.memo_grid[row + col * self.width].is_some()
+    }
 
-            let coord_below = (beam_row + 1, beam_col);
+    #[inline]
+    fn set(&mut self, row: usize, col: usize, value: usize) {
+        self.memo_grid[row + col * self.width] = Some(value);
+    }
 
-            match self.get_node_at_coord(&coord_below) {
-                Some(Node::Empty) => {
-                    self.set_node_at_coord(&coord_below, Node::Beam)?;
-                    new_active_beams.push(coord_below);
+    #[inline]
+    #[allow(dead_code)]
+    fn get(&self, row: usize, col: usize) -> Option<usize> {
+        self.memo_grid[row + col * self.width]
+    }
+
+    fn get_beam_splits(&mut self, (row, col): Coord, quantum: &Quantum) -> Result<usize> {
+        match quantum {
+            Quantum(false) => {
+                if self.contains_key(row, col) {
+                    // We return no more splits, we've been here already
+                    return Ok(0);
                 }
-                Some(Node::Splitter) => {
-                    // We split to the sides
-                    // TODO: beam_col - 1 if beam_col is 0 is bad due to usize
-                    if beam_col > 0 {
-                        let coord_left = (beam_row + 1, beam_col - 1);
-                        self.set_node_at_coord(&coord_left, Node::Beam)?;
-                        new_active_beams.push(coord_left);
-                    }
-                    if beam_col < self.width() - 1 {
-                        let coord_right = (beam_row + 1, beam_col + 1);
-                        self.set_node_at_coord(&coord_right, Node::Beam)?;
-                        new_active_beams.push(coord_right);
-                    }
-                    self.beam_splits += 1;
-                }
-                Some(Node::Beam) => {} // Ignore Beam already there
-                None => {}             // Ignore out of bounds?
-                _ => unimplemented!(),
+            }
+            Quantum(true) => {
+                unimplemented!("Quantum stuff")
             }
         }
-        self.active_beams = new_active_beams;
-        Ok(())
+
+        let coord_below = (row + 1, col);
+
+        let new_splits = match self.get_node_at_coord(&coord_below) {
+            Some(Node::Empty) => {
+                self.set_node_at_coord(&coord_below, Node::Beam)?;
+                self.get_beam_splits(coord_below, quantum)?
+            }
+            Some(Node::Splitter) => {
+                // We split to the sides
+                // NOTE: beam_col - 1 if beam_col is 0 is bad due to usize
+                let left_splits = if col > 0 {
+                    let coord_left = (row + 1, col - 1);
+                    self.set_node_at_coord(&coord_left, Node::Beam)?;
+                    self.get_beam_splits(coord_left, quantum)?
+                } else {
+                    0
+                };
+                let right_splits = if col < self.width() - 1 {
+                    let coord_right = (row + 1, col + 1);
+                    self.set_node_at_coord(&coord_right, Node::Beam)?;
+                    self.get_beam_splits(coord_right, quantum)?
+                } else {
+                    0
+                };
+                left_splits + right_splits + 1
+            }
+            Some(Node::Beam) => {
+                match quantum {
+                    Quantum(false) => 0, // Ignore beam already there
+                    Quantum(true) => unimplemented!("Quantum"),
+                }
+            }
+            None => {
+                match quantum {
+                    Quantum(false) => 0,                        // Ignore beam already there
+                    Quantum(true) => unimplemented!("Quantum"), // Not sure?
+                }
+            } // Ignore out of bounds?
+            _ => unimplemented!(),
+        };
+
+        self.set(row, col, new_splits);
+
+        Ok(new_splits)
     }
 
     fn get_node_at_coord(&self, (row, col): &Coord) -> Option<&Node> {
@@ -121,6 +160,9 @@ impl TryFrom<Vec<Nodes>> for Grid {
     fn try_from(value: Vec<Nodes>) -> Result<Self> {
         // Find the start and set it as the active beam
         // We know that it's going to be on the first row.. so we just look at the first row
+        let height = value.len();
+        let width = value[0].len();
+
         let start_row = 0;
         let start_col = value[0]
             .iter()
@@ -129,8 +171,10 @@ impl TryFrom<Vec<Nodes>> for Grid {
 
         Ok(Self {
             inner: value,
-            active_beams: vec![(start_row, start_col)],
-            beam_splits: 0,
+            start: (start_row, start_col),
+            height,
+            width,
+            memo_grid: vec![None; height * width],
         })
     }
 }
@@ -170,16 +214,18 @@ fn part1(input: &str) -> Result<usize> {
     let node_rows = parse_input(input)?;
     let mut grid = Grid::try_from(node_rows)?;
 
-    while !grid.active_beams.is_empty() {
-        grid.beam_tick()?;
-    }
+    let beam_splits = grid.get_beam_splits(grid.start, &Quantum(false))?;
 
-    Ok(grid.beam_splits)
+    Ok(beam_splits)
 }
 
-fn part2(_input: &str) -> Result<usize> {
-    // let thing = parse_input(input)?;
-    Ok(0)
+fn part2(input: &str) -> Result<usize> {
+    let node_rows = parse_input(input)?;
+    let mut grid = Grid::try_from(node_rows)?;
+
+    let beam_splits = grid.get_beam_splits(grid.start, &Quantum(false))?;
+
+    Ok(beam_splits)
 }
 
 #[cfg(test)]
@@ -195,6 +241,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(TEST_INPUT).unwrap(), 0);
+        assert_eq!(part2(TEST_INPUT).unwrap(), 40);
     }
 }
